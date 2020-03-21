dd060bf23fdbe71a32008b8d76ac5f29294ff5a4
Hayden Bryant <haydenbryant.student@careerdevs.com>
2020-03-21 16:22:20 -0400

    day 17

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,20 +1,39 @@
-import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
+import java.io.*;
 
-public class Solution {
+//Write your code here
+class Calculator{
+    public int power(int n, int p) throws Exception{
+
+        if(n >= 0 && p >= 0){
+            return (int)Math.pow(n, p);
+        }
+        else{
+            throw new Exception("n and p should be non-negative");
+        }
+    }
+}
+
+
+class Solution{
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String S = in.next();
 
-        try{
-            System.out.println(Integer.parseInt(S));
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while (t-- > 0) {
+
+            int n = in.nextInt();
+            int p = in.nextInt();
+            Calculator myCalculator = new Calculator();
+            try {
+                int ans = myCalculator.power(n, p);
+                System.out.println(ans);
+            }
+            catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
         }
-        catch(Exception e){
-            System.out.println("Bad String");
-        }
+        in.close();
     }
 }
