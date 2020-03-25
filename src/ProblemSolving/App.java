7d6611dd2be2453c6184ac0c45e2de08128760db
Hayden Bryant <haydenbryant.student@careerdevs.com>
2020-03-25 12:21:28 -0400

    problem solving time conversion

--- src/ProblemSolving/App.java
+++ src/ProblemSolving/App.java
@@ -1,26 +1,45 @@
 import java.io.*;
-import java.util.*;
-import java.text.*;
 import java.math.*;
+import java.text.*;
+import java.util.*;
 import java.util.regex.*;
 
 public class Solution {
 
-
-
-    static int solveMeFirst(int a, int b) {
-        // Hint: Type return a+b; below
-        return a+b;
+    /*
+     * Complete the timeConversion function below.
+     */
+    static String timeConversion(String s) {
+        if(s.contains("AM"))
+        {
+            String[] arr = s.substring(0, 8).split(":", 2);
+            if(arr[0].contains("12")){
+                arr[0] = "00";
+            }
+            return(arr[0]+":"+arr[1]);
+        } else{
+            String[] arr = s.substring(0, 8).split(":", 2);
+            if(arr[0].contains("12")){
+                return(arr[0]+":"+arr[1]);
+            }
+            int hour = Integer.parseInt(arr[0]);
+            hour += 12;
+            return(hour+":"+arr[1]);
+        }
     }
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int a;
-        a = in.nextInt();
-        int b;
-        b = in.nextInt();
-        int sum;
-        sum = solveMeFirst(a, b);
-        System.out.println(sum);
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = scan.nextLine();
+
+        String result = timeConversion(s);
+
+        bw.write(result);
+        bw.newLine();
+
+        bw.close();
     }
 }
