92e3bf8b9373ffcceaa6b4c3892700ae74753308
HaydenBryant <haydenbryant.student@careerdevs.com>
2020-09-21 15:44:13 -0400

    day 14

--- src/ProblemSolving/App.java
+++ src/ProblemSolving/App.java
@@ -4,37 +4,21 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution {
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int[] a = new int[n];
-        for(int a_i=0; a_i < n; a_i++){
-            a[a_i] = in.nextInt();
-        }
-        // Write Your Code Here
-        int numSwaps = 0;
-        for (int i = 0; i < n; i++) {
-            int swaps = 0;
+class Difference {
+    private int[] elements;
+    public int maximumDifference;
 
-            for (int j = 0; j < n - 1; j++) {
-                if (a[j] > a[j + 1]) {
-                    // Collections.swap(a, a[j], a[j + 1]);
-                    int keep = a[j];
-                    a[j] = a[j + 1];
-                    a[j + 1] = keep;
-                    swaps++;
-                    numSwaps++;
-                }
-            }
+    // Add your code here
 
-            if (swaps == 0) {
-                break;
-            }
-        }
-        System.out.println("Array is sorted in " + numSwaps + " swaps.");
-        System.out.println("First Element: " + a[0]);
-        System.out.println("Last Element: " + a[n-1]);
+    public Difference(int[] elements){
+        this.elements = elements;
     }
-}
+
+    public void computeDifference(){
+        Arrays.sort(elements);
+        this.maximumDifference = Math.abs(elements[elements.length - 1] - elements[0]);
+    }
+
+
+} // End of Difference class
