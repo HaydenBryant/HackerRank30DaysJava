38bffec4fcad64dff6bb209993e0a6fccf1fa98e
HaydenBryant <haydenbryant.student@careerdevs.com>
2020-09-28 19:01:12 -0400

    day 17

--- src/ProblemSolving/App.java
+++ src/ProblemSolving/App.java
@@ -1,20 +1,35 @@
-import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
+import java.io.*;
 
-public class Solution {
+//Write your code here
+class Calculator{
+    public int power(int n, int p) throws Exception{
+        if(n < 0 || p < 0){
+            throw new Exception("n and p should be non-negative");
+        }
+        return (int)Math.pow(n,p);
+    }
+}
+
+class Solution{
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String S = in.next();
 
-        try{
-            System.out.println(Integer.parseInt(S));
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while (t-- > 0) {
+
+            int n = in.nextInt();
+            int p = in.nextInt();
+            Calculator myCalculator = new Calculator();
+            try {
+                int ans = myCalculator.power(n, p);
+                System.out.println(ans);
+            }
+            catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
         }
-        catch(NumberFormatException err){
-            System.out.println("Bad String");
-        }
+        in.close();
     }
 }
