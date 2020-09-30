e707402f9ed4f9c37a252b8809b5f8ae1632d99b
HaydenBryant <haydenbryant.student@careerdevs.com>
2020-09-29 20:07:37 -0400

    day 22. trying recursion. not done

--- src/ProblemSolving/App.java
+++ src/ProblemSolving/App.java
@@ -1,33 +1,83 @@
-import java.io.*;
 import java.util.*;
-
-interface AdvancedArithmetic{
-    int divisorSum(int n);
-}
-class Calculator implements AdvancedArithmetic{
-    public int divisorSum(int n){
-        int sum = 0;
-        for (int i = 1; i*i<=n; i++) {
-            if (n % i == 0) {
-                if (i != n / i){
-                    sum += i;
-                }
-                sum += (n / i);
-            }
-        }
-        return sum;
+import java.io.*;
+class Node{
+    Node left,right;
+    int data;
+    Node(int data){
+        this.data=data;
+        left=right=null;
     }
 }
-class Solution {
+class Solution{
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        scan.close();
+    static int maxHeight = 0;
+    public static int getHeight(Node root){
+        //Write your code here
 
-        AdvancedArithmetic myCalculator = new Calculator();
-        int sum = myCalculator.divisorSum(n);
-        System.out.println("I implemented: " + myCalculator.getClass().getInterfaces()[0].getName() );
-        System.out.println(sum);
+        travler(root, 1);
+
+        //   while(true){
+        //       if(travR.right != null){
+        //           heightR++;
+        //           travR = travR.right;
+        //           continue;
+        //       } else if(travL.left != null){
+        //           heightL++;
+        //           travL = travL.left;
+        //       } else {
+        //           if(heightR > maxHeight){
+        //               maxHeight = heightR;
+        //                 heightR = 0;
+        //           }
+        //           if(heightL > maxHeight){
+        //               maxHeight = heightL;
+        //               heightL = 0;
+        //           }
+        //           break;
+        //       }
+        //   }
+        return maxHeight;
+    }
+
+    public static int travler(Node trav, int height){
+        if(trav.right != null){
+            travler(trav.right, height++);
+        }
+        if(trav.left != null){
+            travler(trav.left, height++);
+        }
+        if(height > maxHeight){
+            maxHeight = height;
+        }
+        return height;
+    }
+
+    public static Node insert(Node root,int data){
+        if(root==null){
+            return new Node(data);
+        }
+        else{
+            Node cur;
+            if(data<=root.data){
+                cur=insert(root.left,data);
+                root.left=cur;
+            }
+            else{
+                cur=insert(root.right,data);
+                root.right=cur;
+            }
+            return root;
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        Node root=null;
+        while(T-->0){
+            int data=sc.nextInt();
+            root=insert(root,data);
+        }
+        int height=getHeight(root);
+        System.out.println(height);
     }
 }
