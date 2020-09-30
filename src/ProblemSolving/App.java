1b5bb23162d8de7401f59c1c3c722a554017877f
HaydenBryant <haydenbryant.student@careerdevs.com>
2020-09-30 13:50:00 -0400

    attempting day 23 using similar method as 22. not done

--- src/ProblemSolving/App.java
+++ src/ProblemSolving/App.java
@@ -10,53 +10,28 @@ class Node{
 }
 class Solution{
 
-    static int maxHeight = 0;
-    public static int getHeight(Node root){
+    static HashMap<Integer, Integer> treeValues = new HashMap<Integer, Integer>();
+    static void levelOrder(Node root){
         //Write your code here
-
         travler(root, 1);
-        return maxHeight;
+
+        // Set<Integer> arr = treeValues.keySet();
+        // System.out.println(arr);
+        for(Integer num : treeValues.keySet()){
+            System.out.println(num);
+        }
+
     }
 
-    public static int travler(Node trav, int height){
+    public static void travler(Node trav, int height){
         if(trav.right != null){
-            travler(trav.right, height++);
+            travler(trav.right, height + 1);
         }
         if(trav.left != null){
-            travler(trav.left, height++);
+            travler(trav.left, height+1);
         }
-        if(height + 1 > maxHeight){
-            maxHeight = height + 1;
-        }
-        return height++;
+        treeValues.put(height + 1, trav.data);
+        return;
     }
 
     public static Node insert(Node root,int data){
-        if(root==null){
-            return new Node(data);
-        }
-        else{
-            Node cur;
-            if(data<=root.data){
-                cur=insert(root.left,data);
-                root.left=cur;
-            }
-            else{
-                cur=insert(root.right,data);
-                root.right=cur;
-            }
-            return root;
-        }
-    }
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        int T=sc.nextInt();
-        Node root=null;
-        while(T-->0){
-            int data=sc.nextInt();
-            root=insert(root,data);
-        }
-        int height=getHeight(root);
-        System.out.println(height);
-    }
-}
