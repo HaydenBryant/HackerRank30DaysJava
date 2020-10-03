3bc5587d663413a07d30fb1d41e6c2deab2af510
HaydenBryant <haydenbryant.student@careerdevs.com>
2020-10-03 13:29:20 -0400

    day 24. works but times out. needs anoter go

--- src/ProblemSolving/App.java
+++ src/ProblemSolving/App.java
@@ -1,70 +1,87 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 class Node{
-    Node left,right;
     int data;
-    Node(int data){
-        this.data=data;
-        left=right=null;
+    Node next;
+    Node(int d){
+        data=d;
+        next=null;
     }
+
 }
-class Solution{
+class Solution
+{
 
-    static HashMap<Integer, Integer> treeValues = new HashMap<Integer, Integer>();
-    // static Stack<Integer> stack = new Stack<Integer>();
-// static ArrayList<Integer> list = new ArrayList<Integer>();
-    static int maxHeight = 0;
-    static void levelOrder(Node root){
+    public static Node removeDuplicates(Node head) {
         //Write your code here
-        travler(root, 1);
-        for(int i = 0; i <= maxHeight; i++){
-            for(int key : treeValues.keySet()){
-                if(treeValues.get(key) == i){
-                    System.out.print(key + " ");
+        int last = head.data;
+        Node trav = head.next;
+        Node cleanList = new Node(head.data);
+
+        while(true){
+            if(trav.data == last){
+                trav = trav.next;
+                continue;
+            }
+            Node p = new Node(trav.data);
+            if(cleanList.next==null){
+                cleanList.next = p;
+            } else {
+                Node start = cleanList;
+                while(start.next!=null){
+                    start = start.next;
                 }
+                start.next=p;
             }
+
+            last = trav.data;
+            if(trav.next == null){
+                break;
+            }
+            trav = trav.next;
+
         }
+        return cleanList;
+
     }
 
-    public static void travler(Node trav, int height){
-        if(trav.right != null){
-            travler(trav.right, height + 1);
-        }
-        if(trav.left != null){
-            travler(trav.left, height+1);
-        }
-        if(height + 1 > maxHeight){
-            maxHeight = height;
-        }
-        treeValues.put(trav.data, height);
-        return;
-    }
+    public static  Node insert(Node head,int data)
+    {
+        Node p=new Node(data);
+        if(head==null)
+            head=p;
+        else if(head.next==null)
+            head.next=p;
+        else
+        {
+            Node start=head;
+            while(start.next!=null)
+                start=start.next;
+            start.next=p;
 
-    public static Node insert(Node root,int data){
-        if(root==null){
-            return new Node(data);
         }
-        else{
-            Node cur;
-            if(data<=root.data){
-                cur=insert(root.left,data);
-                root.left=cur;
-            }
-            else{
-                cur=insert(root.right,data);
-                root.right=cur;
-            }
-            return root;
+        return head;
+    }
+    public static void display(Node head)
+    {
+        Node start=head;
+        while(start!=null)
+        {
+            System.out.print(start.data+" ");
+            start=start.next;
         }
     }
-    public static void main(String args[]){
+    public static void main(String args[])
+    {
         Scanner sc=new Scanner(System.in);
+        Node head=null;
         int T=sc.nextInt();
-        Node root=null;
         while(T-->0){
-            int data=sc.nextInt();
-            root=insert(root,data);
+            int ele=sc.nextInt();
+            head=insert(head,ele);
         }
-        levelOrder(root);
+        head=removeDuplicates(head);
+        display(head);
+
     }
 }
