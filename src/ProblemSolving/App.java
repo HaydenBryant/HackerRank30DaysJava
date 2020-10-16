6f8ff3f2a185273d7d13a8f434b3e710c563af65
HaydenBryant <haydenbryant.student@careerdevs.com>
2020-10-16 13:41:48 -0400

    tried different approach but doesnt work with how the input comes in. would if i read all numbers. need new method

--- src/ProblemSolving/App.java
+++ src/ProblemSolving/App.java
@@ -8,6 +8,7 @@ public class Solution {
         Scanner in = new Scanner(System.in);
 
         int iterations = in.nextInt();
+        ArrayList<Integer> primes = new ArrayList<Integer>();
 
         for(int i = iterations; i > 0; i--){
             int possPrime = in.nextInt();
@@ -15,9 +16,15 @@ public class Solution {
                 System.out.println("Prime");
                 continue;
             }
-            if((possPrime - 1) != -1 % possPrime ||
-                    (possPrime - 1) != (possPrime-1) % possPrime){
+            if(possPrime % 2 != 0){
+                for(int prime : primes){
+                    if(possPrime % prime == 0){
+                        System.out.println("Not prime");
+                        break;
+                    }
+                }
                 System.out.println("Prime");
+                primes.add(possPrime);
                 continue;
             }
             System.out.println("Not prime");
