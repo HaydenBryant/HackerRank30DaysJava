7c1930d8bf311067b89fa436f745ae4c1c0671d1
Hayden Bryant <haydenbryant.student@careerdevs.com>
2021-02-11 20:01:52 -0500

    day 22

--- src/ProblemSolving/Day22.java
+++ src/ProblemSolving/Day22.java
@@ -1,5 +1,24 @@
 package ProblemSolving;
 
 public class Day22 {
+    static int maxHeight = 0;
+    public static int getHeight(Node root){
+        //Write your code here
 
+        travler(root, 1);
+        return maxHeight;
+    }
+
+    public static int travler(Node trav, int height){
+        if(trav.right != null){
+            travler(trav.right, height++);
+        }
+        if(trav.left != null){
+            travler(trav.left, height++);
+        }
+        if(height + 1 > maxHeight){
+            maxHeight = height + 1;
+        }
+        return height++;
+    }
 }
